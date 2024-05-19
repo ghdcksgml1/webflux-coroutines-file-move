ee3888df6b915913b76bcbc8f3f429ec8ef5697b
ghdcksgml1 <ghdcksgml2@naver.com>
2024-05-19 16:05:00 +0900

    refact: delay 제거

--- webflux-coroutines/src/main/kotlin/io/file/storage/file/FileHelper.kt
+++ webflux-coroutines/src/main/kotlin/io/file/storage/file/FileHelper.kt
@@ -1,7 +1,6 @@
 package io.file.storage.file
 
 import kotlinx.coroutines.coroutineScope
-import kotlinx.coroutines.delay
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Repository
 import java.nio.file.Files
@@ -13,7 +12,6 @@ class FileHelper {
 
     suspend fun moveFile(command: FileMoveCommand) = coroutineScope {
         log.info("[{}]에서 [{}]로 이동합니다.", command.from.path.normalize(), command.to.path.normalize())
-//        delay(10)
         FilePath(Files.move(command.from.path, command.to.path))
     }
 }
